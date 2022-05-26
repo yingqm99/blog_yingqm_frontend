eb8ea1d30e0ecf17da90a5d187ed478b5f7ebd0d
Qiming <yingqm@umich.edu>
2022-05-26 09:23:41 -0400

    mod: main page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleSm">Qiming Ying</span>
         <span className="headerTitleLg">BLOG</span>
       </div>
       <img
