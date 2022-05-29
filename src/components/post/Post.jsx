794cd66ef2501cdd222b0728f4ab9d07181bcd10
Qiming <yingqm@umich.edu>
2022-05-29 01:06:37 -0400

    mod: post style

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,57 +1,35 @@
 import { Link } from "react-router-dom";
 import "./post.css";
 import { useSelector } from 'react-redux';
-import { Box } from "@mui/system";
+import { Card, CardContent } from "@mui/material";
+import { CardActionArea } from "@mui/material";
+import { Typography } from "@mui/material";
 
 export default function Post({title}) {
   const userName = useSelector(state => state.user);
   console.log(userName);
 
-  const handleMouseOver = (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-    console.log(e.target.parent);
-    e.target.parent.parent.style.borderStyle = "solid";
-  }
 
   return (
-    <div className="post">
-      {/* <img
-        className="postImg"
-        src={img}
-        alt=""
-      /> */}
-      <div className="postInfo">
-        {/* <div className="postCats">
-          <span className="postCat">
-            <Link className="link" to="/posts?cat=Music">
-              Music
+    <Card sx={{height: "150", mt: 1}}>
+    
+      <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div">
+            <Link to="/post/{title}" className="link">
+              
+                {title}
             </Link>
-          </span>
-          <span className="postCat">
-            <Link className="link" to="/posts?cat=Music">
-              Life
-            </Link>
-          </span>
-        </div> */}
-        <div className="postTitle" onMouseOver={handleMouseOver}>
-          <Link to="/post/{title}" className="link">
-            {title}
-          </Link>
-          
-          <span className="postDate">1 hour ago</span>
-          <span className="postDate">{userName}</span>
-        </div>
-        {/* <hr /> */}
-        
-      </div>
+            </Typography>
+            <Typography variant="body3" color="text.secondary">
+            <span className="postDate">1 hour ago</span>
+            <span className="postDate">{userName}</span>
+            </Typography>
+            
+          </CardContent>
+      </CardActionArea>
       
-      {/* <p className="postDesc">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
-        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
-        atque, exercitationem quibusdam, reiciendis odio laboriosam?
-      </p> */}
-    </div>
+    </Card>
+    
   );
 }
