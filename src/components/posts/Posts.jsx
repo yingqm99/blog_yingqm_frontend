e7dc643d7085a53ad370b4662fcd9a4bbd1f12b7
Qiming <yingqm@umich.edu>
2022-05-29 15:47:07 -0400

    mod: blog display

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -2,19 +2,52 @@ import Post from "../post/Post";
 import "./posts.css";
 import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router';
-import { useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function Posts() {
   const url = useLocation();
   const blogs = useSelector(state => state.blogs);
+  const user = useSelector(state => state.user);
 
-  console.log(blogs)
+  console.log(blogs);
+
+  const [allBlogs, setAllBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/all_blogs", {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Access-Control-Allow-Origin':'http://localhost:8080',
+        'Access-Control-Allow-Credentials': true,
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => {
+      if (!response.ok) throw Error(response.status);
+      return response.json();
+    }).then((data) => {
+      if (!data.status) throw Error("load main page failed");
+
+      if (url.pathname === '/') {
+        let all_blogs = [];
+        data.blogs.map((blog) => {
+          all_blogs.push(blog.name);
+        })
+        setAllBlogs(all_blogs);
+      } else {
+        setAllBlogs(blogs);
+      }
+    }).catch(err => {
+      console.log(err);
+    });
+  }, [user, url.pathname, blogs])
+
 
 
   return (
     <div className="posts">
       {
-        blogs.map((blog, i) => {
+        allBlogs.map((blog, i) => {
           return <Post title={blog} />
         })
       }
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -3,10 +3,12 @@ import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Header from "../../components/header/Header";
 import "./homepage.css";
+import { useEffect } from "react";
 
 export default function Homepage() {
   const location = useLocation();
   console.log(location);
+
   return (
     <>
       <div className="home">
